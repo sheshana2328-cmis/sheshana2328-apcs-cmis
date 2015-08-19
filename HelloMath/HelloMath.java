aa6191e38a07cd70f654799d0bc6a2c144ebb91b
sheshana2328-cmis <sheshana2328@cmis.ac.th>
2015-08-19 07:10:51 +0000

    Update HelloMath.java

--- HelloMath/HelloMath.java
+++ HelloMath/HelloMath.java
@@ -14,6 +14,7 @@ public class HelloMath
          
          System.out.println (n1 + n2);
          System.out.println (n1 - n2);
+         System.out.println (n1 - n2)
         
             
         }
