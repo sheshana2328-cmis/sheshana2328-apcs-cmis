92153dd9246754fbbf50a9b0dd341dc381b62d41
cmis <cmis@cmis-hs14.(none)>
2015-08-24 09:56:09 +0700

    Finished F1 started F2

---
+++ Fundamentals1/Fundamentals1/Fundamentals1.java
@@ -0,0 +1,13 @@
+public class Fundamentals1
+    {
+     public static void main ( String[] args ) 
+        {
+         System.out.println("My Favorites:");
+         
+         System.out.println("Favorite Food - Meatloaf ");
+         System.out.println("Favorite Colour - Purple");
+         System.out.println("Favorite Movie - Whiplash");
+         System.out.println("Favorite Musical Group - Twenty One Pilots");
+         
+        } //end method main
+    }//end Fundamentals1 class
---
+++ Fundamentals2/Fundamentals2/Fundamentals2.java
@@ -0,0 +1,9 @@
+
+
+public class Fundamentals2
+    {
+     public static void main ( String[] args )
+        {
+          String
+        }
+    }
