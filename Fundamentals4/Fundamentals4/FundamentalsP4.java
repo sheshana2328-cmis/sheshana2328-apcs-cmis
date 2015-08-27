5604509f0313316532fc742a3466280602be2143
cmis <cmis@cmis-hs14.(none)>
2015-08-27 11:54:15 +0700

    fin FP4 started FP5

--- Fundamentals4/Fundamentals4/FundamentalsP4.java
+++ Fundamentals4/Fundamentals4/FundamentalsP4.java
@@ -2,6 +2,9 @@ public class FundamentalsP4
     {
      public static void main ( String[] args )
         {
-            
+        for( int num1 = 1;num1 < 11; num1 ++ )
+            {
+             System.out.println( num1 + "\t" + num1*num1 + "\t"  + (int)(num1+(num1*Math.random())) );
+            } // end for
         } // end main method
     } // end class FundamentalsP4
---
+++ Fundamentals5/Fundamentals5/FundamentalsP5.java
@@ -0,0 +1,7 @@
+public class FundamentalsP5
+    {
+     public static void main ( String[] args )
+        {
+            
+        } // end main
+    } // end class
