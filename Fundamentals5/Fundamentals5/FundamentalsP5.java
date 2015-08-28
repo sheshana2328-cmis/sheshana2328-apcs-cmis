3834bd34ac643e565f00dcf6eb7c0acd87baffee
cmis <cmis@cmis-hs14.(none)>
2015-08-28 09:57:41 +0700

    worked on FP5

--- Fundamentals5/Fundamentals5/FundamentalsP5.java
+++ Fundamentals5/Fundamentals5/FundamentalsP5.java
@@ -2,6 +2,13 @@ public class FundamentalsP5
     {
      public static void main ( String[] args )
         {
+            String fm1 = "The Shining";
+            String fm2 = "Hot Fuzz";
+            String fm3 = "Ender's Game";
+            String fs1 = "Letting in";
+            String fs2 = "Me";
+            String fs3 = "Thirteen Thirtyfive";
             
+            String names[] = {}
         } // end main
     } // end class
