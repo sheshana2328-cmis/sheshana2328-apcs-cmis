4663f6e29b6a2d093028279edd0158c089eaef43
cmis <cmis@cmis-hs14.(none)>
2015-09-01 14:07:06 +0700

    fin P6 st P7

--- Fundamentals6/Fundamentals6/FundamentalsP6.java
+++ Fundamentals6/Fundamentals6/FundamentalsP6.java
@@ -1,7 +1,15 @@
+package Fundamentals6;
+
 public class FundamentalsP6
     {
         public static void main ( String[] args )
             {
-                int 1 =
+             int[] randomNumbers = new int[50];
+             
+             for ( int index = 0; index < randomNumbers.length; index++ )
+                {
+                 randomNumbers[ index ] = (int) ( Math.random()*100 );
+                 System.out.print( randomNumbers[ index ] + " " );
+                } // end for
             } // end method main
     } // end class FundamentalsP6
---
+++ Fundamentals7/Fundamentals7/FundamentalsP7.java
@@ -0,0 +1,25 @@
+public class FundamentalsP7
+    {
+        public static void main ( String[] args )
+            {
+             int[] ranNum = new int[10];
+             
+             for ( int index = 0; index < ranNum.length; index++ )
+                {
+                    ranNum[ index ] = (int) ( Math.random()*100 );
+                    if ( ranNum >= (Math.randome()*100)
+                        {
+                            int neg = ranNum*-1
+                        }
+                    else{
+                    }
+                }
+             while ( index < ranNum.length)
+                {
+                }
+             
+             for ()
+                {
+                }
+            } // end method main
+    } // end class
