7d8b7669b8a3ec4f0a84456b7f22546cbb6df045
cmis <cmis@cmis-hs14.(none)>
2015-09-04 11:56:22 +0700

    Test1-4

--- TestOne/TestOne.java
+++ TestOne/TestOne.java
@@ -7,15 +7,31 @@ public class TestOne
                 String int1 = JOptionPane.showInputDialog("Enter an integer:");
                 
                 int input = Integer.parseInt(int1);
+                double doub = (double) input; // convert into double
                 
                 int[] array = new int [input];
                 
+                
                 for ( int index = 0; index < array.length; index++ )
                     {
-                       array[ index ] = (int) (Math.random()*(50+1));
-                       System.out.println(array[index] + " ");
-
+                       array[ index ] = (double) (Math.random()*(50+1));
+                       System.out.println( array[index] + ":");
+                       if( input % 2 == 0 )// find even
+                       {
+                           System.out.print("E"*array[index]);
+                       }
+                       else // find odds
+                       {
+                           System.ot.println("O"*array[index]);
+                       }
+                   
+                       System.out.println( array[index] + ":" ); 
                     }
+                while( index < array.length )
+                    {
+                       
+                    }
+                   
                 
             } // end method main
     } // end class TestOne
