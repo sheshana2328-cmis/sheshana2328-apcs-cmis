cc581f02500225b914a1f54a0493b33afd4d1a30
cmis <cmis@cmis-hs14.(none)>
2015-09-04 11:30:28 +0700

    test1-2

--- TestOne/TestOne.java
+++ TestOne/TestOne.java
@@ -4,6 +4,16 @@ public class TestOne
     {
         public static void main ( String[] args )
             {
-                String Int1 = JOptionPane.showInputDialog("Enter an integer:");
+                String int1 = JOptionPane.showInputDialog("Enter an integer:");
+                
+                int input = Integer.parseInt(int1);
+                
+                int[] array = new int [input];
+                
+                for ( int index = 0; index < array.length; index++ )
+                    {
+                       System.out.println( array[index] ); 
+                    }
+                
             } // end method main
     } // end class TestOne
