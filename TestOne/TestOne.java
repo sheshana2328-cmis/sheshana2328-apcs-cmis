5bf37bf029214a008192a8fae94e771c8fdc8f53
cmis <cmis@cmis-hs14.(none)>
2015-09-04 11:17:30 +0700

    test1-1

---
+++ TestOne/TestOne.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class TestOne
+    {
+        public static void main ( String[] args )
+            {
+                String Int1 = JOptionPane.showInputDialog("Enter an integer:");
+            } // end method main
+    } // end class TestOne
