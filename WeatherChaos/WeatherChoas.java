24b06b46c3ea8d121acccb5d8997e98851c60db0
cmis <cmis@cmis-hs14.(none)>
2015-09-24 14:21:34 +0700

    MCC4

--- WeatherChaos/WeatherChoas.java
+++ WeatherChaos/WeatherChoas.java
@@ -13,30 +13,23 @@ public class WeatherChoas
                 int[] numdays = new int[inp];
                 int[] swing = new int[inp];
                 String headings;
+                //x = (int) (Math.random()*200)-100;
                 
                 //String addto; 
                 String[] heading = {"day","temp","swing","description"};
                 
                 if ( inp >= 1 && inp <= 31 )
                 {
-                    for (int i = 0; i < heading.length; i++)
+                   for (int i = 0; i < heading.length; i++)
                     {
                         System.out.print(heading[i]+ "\t");
                     }
                     System.out.println("\n");
                     for (int i = 0; i < inp; i++ )
                     {
-                       System.out.println(i+1);
-                    }
-                    System.out.println("\n"); //i put a line to seperate for now
-                    for ( int i = 0; i < inp; i++ )
-                    {
-                        x = (int) (Math.random()*200)-100;
-                        System.out.println(x);
-                    }
-                    System.out.println("\n"); //i put a line to seperate for now
-                    for ( int i = 0; i < inp; i++ ) // for description
-                    {
+                      System.out.println(i+1);
+                      x = (int) (Math.random()*200)-100;
+                        System.out.print(x + "\t\t\t");
                         if ( x < 0 )
                         {
                             System.out.println("freezing");
@@ -45,25 +38,51 @@ public class WeatherChoas
                         {
                             System.out.println("chilly");
                         }
-                        if ( x > 15 && x < 30 )
+                        if ( x > 16 && x < 30 )
                         {
                             System.out.println("comfortable");
                         }
                         if ( x > 31 && x < 40 )
                         {
-                            System.out.println("hot");
+                           System.out.println("hot");
                         }
-                        if (  x < 15 )
+                        if ( x > 40 )
                         {
                             System.out.println("ARGHHH");
+                         }
+                   }
+                    System.out.println("\n"); //i put a line to seperate for now
+                    
+                   
+                   /*for ( int i = 0; i < inp; i++ ) // for description
+                    {
+                        x = (int) (Math.random()*200)-100;
+                        System.out.print(x + "\t\t\t");
+                        if ( x < 0 )
+                        {
+                            System.out.println("freezing");
                         }
-                    }
-                     
-                } // end if
-                
+                        if ( x > 0 && x < 15 )
+                        {
+                            System.out.println("chilly");
+                        }
+                        if ( x > 16 && x < 30 )
+                        {
+                            System.out.println("comfortable");
+                        }
+                        if ( x > 31 && x < 40 )
+                        {
+                           System.out.println("hot");
+                        }
+                        if ( x > 40 )
+                        {
+                            System.out.println("ARGHHH");
+                         }
+                    }*/
+                }
                 else 
                 {
                     System.out.println("That's not a valid number of days!");
                 }
-            } // end method main
-    } // end class WeatherChaos
+                } // end 
+            } // end 
---
+++ WeatherChaosNote/WeatherChoas.java
@@ -0,0 +1,38 @@
+
+import javax.swing.JOptionPane;
+
+public class WeatherChoas
+    {
+        public static void main ( String[] argv )
+            {
+                String input = JOptionPane.showInputDialog("Enter an integer between 1 - 31:");
+                
+                int inp = Integer.parseInt(input);
+                int x;
+                int y;
+                
+                int[] numdays = new int[inp];
+                int[] swing = new int[inp];
+                String headings;
+                
+                //String addto; 
+                String[] heading = {"day","temp","swing","description"};
+                
+                if ( inp >= 1 && inp <= 31 )
+                {
+                    for (int i = 0; i < heading.length; i++)
+                    {
+                        System.out.print(heading[i]+ "\t");
+                        int ind = i+1;
+                        addto += ind;
+                        //System.out.println(i+1);
+                        /*x = (int) (Math.random()*200)-100;
+                        System.out.println(x);*/
+                    }
+                }
+                else 
+                {
+                    System.out.println("That's not a valid number of days!");
+                }
+            } // end method main
+    } // end class WeatherChaos
