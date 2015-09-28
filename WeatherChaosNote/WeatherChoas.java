89e4e2af3ff4c42bf2115ba41bd2ff46a219771e
cmis <cmis@cmis-hs14.(none)>
2015-09-28 09:56:44 +0700

    WCC5

--- WeatherChaosNote/WeatherChoas.java
+++ WeatherChaosNote/WeatherChoas.java
@@ -12,16 +12,24 @@ public class WeatherChoas
                 int day;
                 String des;
                
+                int min = 99;
+                int max = 0;
+                int MinDay = 0;
+                int MaxDay = 0;
                 
                 if (inp <= 31 && inp >= 1)
                 {
-                    System.out.println("Days" + "\tTemp" + "\tSwing" + "\tDescription");
+                    System.out.println("Days" + "\tTemp" + "\tSwing" + "\tDescription" + "\n");
                     
                     for ( int i = 0; i < inp; i++ )
                     {
                         int x = (int) (Math.random()*200)-100;
                         temp[i] = x;
                         day = i+1;
+                        des = " ";
+                        
+                        
+                        
                         if ( i > 0 )
                             {
                                 swing = Math.abs(temp[1] - temp[i-1]);
@@ -32,7 +40,7 @@ public class WeatherChoas
                             }
                         if ( x < 0 )
                             {
-                             des = "freezing";    
+                              des = "freezing";    
                             }
                         if ( x > 0 && x < 15 )
                             {
@@ -48,11 +56,27 @@ public class WeatherChoas
                             }
                         if ( x > 40 )
                             {
-                             des = "AAAAAUUUUHGHHH!";
+                              des = "AAAAAUUUUHGHHH!";
                             }
                         System.out.println(day + "\t" + temp[i] + "\t" + swing + "\t" + des );
+                        
+                        if ( x < min )
+                            {
+                                min = x ;
+                                MinDay = day;
+                             }
+            
+                        if ( x > max )
+                             {
+                                 max = x;
+                                 MaxDay = day;
+                              }
                     }
-                }// end if
+                    
+                    System.out.println("\nMax: Temp " + max + " on day:" + MaxDay + "\nMin: Temp" + min + " on day:" + MinDay);
+            
+
+                    }// end if
                 else
                 {
                     System.out.println("That is not a valid number of days!");
