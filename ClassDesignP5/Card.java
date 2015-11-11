24a72196124eb2d2e447f22aebddae40a324576e
sheshana2328-cmis <sheshana2328@cmis.ac.th>
2015-11-11 09:22:01 +0700

    trying to fix some issuses with laptop

--- ClassDesignP5/Card.java
+++ ClassDesignP5/Card.java
@@ -1,33 +1,18 @@
-
-/**
- * Write a description of class Card here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Card
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Card
-     */
-    public Card()
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+        private int value;
+        private String suit;
+        
+        public Card()
+            {
+             value = 3;
+             suit = new String ("Spades");
+            }
+            
+        public String toString()
+            {
+             String output = new String();
+             output = value + " of " + suit;
+             return output;
+            }
+    }// end class Card
---
+++ ClassDesignP5/CardDriver.java
@@ -0,0 +1,9 @@
+public class CardDriver
+    {
+        public static void main (String[] argv)
+            {
+                Card Card1 = new Card();
+                
+                System.out.println(Card1);
+            }
+    }//end class CardDriver
