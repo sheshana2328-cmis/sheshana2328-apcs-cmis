96236108233273a13124d68f9cac141cd77974e4
cmis <cmis@cmis-hs14.(none)>
2015-11-11 09:55:36 +0700

    fin CM1

--- ClassMethod1/SongDriver.java
+++ ClassMethod1/SongDriver.java
@@ -7,17 +7,13 @@ public class SongDriver
                 String user = JOptionPane.showInputDialog("Enter a username:");
                 String pass = JOptionPane.showInputDialog("Enter a password:");
                 
-                
-                //int ps = Integer.parseInt(pass);
-                //int yr = Integer.parseInt(input);
-                
                 if ( user.equals("shay")&&pass.equals("123") )
                     {
                         String input = JOptionPane.showInputDialog("Enter Released Date for 'Communist Daughter':");
                         int yr = Integer.parseInt(input);
                         
                         Song Song1 = new Song();
-                        Song Song2 = new Song("Matilda", 1204410, 3.50, 2012); 
+                        Song Song2 = new Song("Matilda", 1204410, 210, 2012); 
                 
                         Song1.setYearReleased( yr );
                         System.out.println( Song1 + "\n");
@@ -25,12 +21,9 @@ public class SongDriver
                     }
                 else
                     {
-                        System.out.println("Login unsuccessful… try again");
+                        System.out.println("Login unsuccessful… Try again");
                     }
 
-                /*System.out.println( Song1.getYearReleased() );
-                Song1.setYearReleased( 1998 );
-                System.out.println( Song1.getYearReleased() );
-                System.out.println( Song1 + "\n");*/
+               
             }
     }//end class SongDriver
