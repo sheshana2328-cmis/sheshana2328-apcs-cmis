fad2268409e77eff3e023993c4ae7baa3ec4d51b
sheshana2328-cmis <sheshana2328@cmis.ac.th>
2015-12-11 21:30:55 +0700

    Fin ClassMethods

---
+++ ClassMethod6/Password.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class Password
+{
+    public static void main ( String[] args )
+    {
+        int count = 0;
+        String pass = new String(JOptionPane.showInputDialog("Enter your password: [It has to have a length of at least 6 characters and at least one non-alphanumeric character (+, -, *, /, or @)]"));
+        
+        for(int i=0; i<pass.length(); i++)
+        {
+            if(pass.substring(i).equals("+")||pass.substring(i).equals("-")||pass.substring(i).equals("*")||pass.substring(i).equals("/")||pass.substring(i).equals("@"))
+            {
+                count+=1;
+            }
+        }
+        if (pass.length() < 6 || count == 0)
+        {
+            System.out.println("This didn't work, try again?");
+        }
+        else
+        {
+            System.out.println("Congratulations! It worked!");
+        }
+        
+    } // end method Main
+} //end class Password
