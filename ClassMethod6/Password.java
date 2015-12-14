7500d299e5c2392feb8a45caa2958cfaec2f092d
sheshana2328-cmis <sheshana2328@cmis.ac.th>
2015-12-14 14:38:00 +0700

    Done with EVERYTHANG MWHAHAHAH

--- ClassMethod6/Password.java
+++ ClassMethod6/Password.java
@@ -6,12 +6,17 @@ public class Password
     {
         int count = 0;
         String pass = new String(JOptionPane.showInputDialog("Enter your password: [It has to have a length of at least 6 characters and at least one non-alphanumeric character (+, -, *, /, or @)]"));
-        
+
         for(int i=0; i<pass.length(); i++)
         {
             if(pass.substring(i).equals("+")||pass.substring(i).equals("-")||pass.substring(i).equals("*")||pass.substring(i).equals("/")||pass.substring(i).equals("@"))
             {
                 count+=1;
+                //System.out.println(count);
+            }
+            else
+            {
+                count = count;
             }
         }
         if (pass.length() < 6 || count == 0)
@@ -22,6 +27,6 @@ public class Password
         {
             System.out.println("Congratulations! It worked!");
         }
-        
+
     } // end method Main
 } //end class Password
